db40667b4a4a5f8f761ada86d77c8d517470cfca
Carrie Forde <carrie@carrieforde.com>
2019-05-20 19:21:46 -0700

    Adds some real content to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Site from '../components/Site/Site';
-import Image from '../components/image';
+
 import SEO from '../components/SEO/SEO';
 
 const IndexPage = () => (
   <Site>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <section className="hero">
+      <header className="page-header">
+        <h1 className="page-title">
+          Hello, I’m Carrie!{' '}
+          <span className="emoji" role="img" aria-label="waving hand">
+            👋
+          </span>
+        </h1>
+        <p className="page-description">
+          I am a front end software engineer in San Mateo, California who builds
+          elegant, maintainable, and performant websites with a modern tech
+          stack & WordPress.
+        </p>
+        <p>
+          Send me a message{' '}
+          <a href="mailto:carrie@carrieforde.com">carrie@carrieforde.com</a>{' '}
+        </p>
+      </header>
+    </section>
   </Site>
 );
 
