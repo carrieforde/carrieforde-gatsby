f9eea499ae01cb622d55b970916157e8237df316
Carrie Forde <carrie@carrieforde.com>
2019-05-26 18:33:39 -0700

    Enables offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,9 +99,7 @@ module.exports = {
         trackingId: process.env.GATSBY_GA_ID || null
       }
     },
-    'gatsby-plugin-netlify'
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gatsby-plugin-netlify',
+    `gatsby-plugin-offline`
   ]
 };
