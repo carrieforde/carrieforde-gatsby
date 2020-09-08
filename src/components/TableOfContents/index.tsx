c310ae7f8d48a63bb20c1a96e497a2583f56aa14
Carrie Forde <carrie@carrieforde.com>
2020-09-07 18:00:20 -0700

    Adds accessibility-friendly way to close TOC

--- src/components/TableOfContents/index.tsx
+++ src/components/TableOfContents/index.tsx
@@ -1,5 +1,5 @@
 import ReactHtmlParser from 'react-html-parser';
-import React, { useState, ReactElement } from 'react';
+import React, { useState, ReactElement, useEffect } from 'react';
 import styles from './TableOfContents.module.css';
 import Chevron from '../../icons/chevron-circle-left-light.svg';
 
@@ -11,6 +11,34 @@ function TableOfContents({
   tableOfContents,
 }: TableOfContentsProps): ReactElement {
   const [tocOpen, setTocState] = useState(false);
+  const handleClick = () => {
+    if (tocOpen) {
+      setTocState(!tocOpen);
+    }
+
+    if (
+      !tocOpen &&
+      document.activeElement.classList.contains(styles.tocButton)
+    ) {
+      setTocState(!tocOpen);
+    }
+  };
+  const handleKeyup = (e: KeyboardEvent) => {
+    if (tocOpen && e.keyCode === 27) {
+      setTocState(!tocOpen);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+    document.addEventListener('keyup', handleKeyup);
+
+    return function cleanup() {
+      document.removeEventListener('click', handleClick);
+      document.removeEventListener('keyup', handleKeyup);
+    };
+  }, [tocOpen]);
+
   return (
     <div
       className={`${styles.tableOfContents} ${tocOpen ? styles.tocOpen : null}`}
