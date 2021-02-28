a38c4186c818920910cf4bd667277d430bd79266
Carrie Forde <carrie@carrieforde.com>
2021-02-28 09:41:20 -0800

    Adds privacy policy

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,17 @@
+import { Link } from 'gatsby';
 import React from 'react';
 import SocialNav from '../SocialNav';
 import styles from './footer.module.css';
+import utils from '../../styles/utilities.module.css';
 
 const Footer: React.FC<unknown> = () => (
   <footer className={styles.footer}>
     <p>
       Copyright &copy; 2013 &ndash; {new Date().getFullYear()} Carrie Forde.
+      <span className={styles.bullet}>&bull;</span>
+      <Link to="/privacy" className={utils.noBackgroundLink}>
+        Privacy
+      </Link>
     </p>
     <SocialNav />
   </footer>
