44db91c58b3e7ca5f90eb33031a084794ba9f20e
Carrie Forde <carrie@carrieforde.com>
2021-02-28 09:44:14 -0800

    Tags privacy

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,13 +3,18 @@ import React from 'react';
 import SocialNav from '../SocialNav';
 import styles from './footer.module.css';
 import utils from '../../styles/utilities.module.css';
+import { gtm } from '../../utils/analytics';
 
 const Footer: React.FC<unknown> = () => (
   <footer className={styles.footer}>
     <p>
       Copyright &copy; 2013 &ndash; {new Date().getFullYear()} Carrie Forde.
       <span className={styles.bullet}>&bull;</span>
-      <Link to="/privacy" className={utils.noBackgroundLink}>
+      <Link
+        to="/privacy"
+        className={utils.noBackgroundLink}
+        onClick={() => gtm({ event: 'click', label: 'Privacy' })}
+      >
         Privacy
       </Link>
     </p>
